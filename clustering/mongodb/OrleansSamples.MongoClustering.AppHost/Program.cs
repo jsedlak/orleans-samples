08eee27a7257c75fb47bf72005ec95d492b8ca80
John Sedlak <john.sedlak@outlook.com>
2025-01-19 08:41:35 -0500

    Adds initial run of Mongo Clustering

---
+++ clustering/mongodb/OrleansSamples.MongoClustering.AppHost/Program.cs
@@ -0,0 +1,16 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var mongo = builder.AddMongoDB("mongo");
+var clustering = mongo.AddDatabase("clustering");
+
+var orleans = builder.AddOrleans("orleans")
+    .WithClustering(clustering)
+    .WithMemoryGrainStorage("Default");
+
+builder.AddProject<Projects.OrleansSamples_MongoClustering_Silo>("silo")
+    .WithReplicas(3)
+    .WithReference(orleans)
+    .WithReference(mongo)
+    .WaitFor(mongo);
+
+builder.Build().Run();
