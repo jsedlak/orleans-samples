adf506442115abc197fa5df95331303d44e3f211
Rinke Blootens <r.blootens@safetyct.com>
2025-06-10 15:14:43 +0200

    Add readonly attribute to allow concurrent cache reading.

--- patterns/cached-silo-reads/CachedSiloReads.SiloHost/GrainModel/IReceivingCachingWeatherGrain.cs
+++ patterns/cached-silo-reads/CachedSiloReads.SiloHost/GrainModel/IReceivingCachingWeatherGrain.cs
@@ -1,9 +1,11 @@
 using CachedSiloReads.SiloHost.Model;
+using Orleans.Concurrency;
 
 namespace CachedSiloReads.SiloHost.GrainModel;
 
 public interface IReceivingCachingWeatherGrain : IGrainWithStringKey
 {
+    [ReadOnly]
     Task<WeatherForecast?> GetForecast();
 
     Task Deactivate();
